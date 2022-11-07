08cc7660aa00f7c05024ef144d1a0cc7258e4662
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2022-11-07 23:02:23 +0600

    Update expense_calculator.c

--- SPL(FALL2022)/Ass2/expense_calculator.c
+++ SPL(FALL2022)/Ass2/expense_calculator.c
@@ -5,32 +5,41 @@ int main(){
     int expense[] = {50, 30, 40, 90, 50, 30, 40, 20, 10, 22, 33, 44};
     char months[12][4] = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"}; 
     
-    char lb[4];
-    char ub[4];
+    while (1){
+        fflush(stdin);
+        char lb[4];
+        char ub[4];
 
-    scanf("%[^\n]s", lb);
-    fflush(stdin);
-    scanf("%[^\n]s", ub);
-
-    int ub_idx = 0;
-    int lb_idx = 0;
-
-    for (int i=0; i<12; i++){
-        //printf("%s == %s or %s\n", months[i], ub, lb);
-        if (strcmp(months[i], ub) == 0){
-            ub_idx = i;
+        scanf("%[^\n]s", lb);
+        fflush(stdin);
+        if (strcmp("exit", lb) == 0){
+            break;
         }
-        if (strcmp(months[i], lb) == 0){
-            lb_idx = i;
-        }
-    }
-    printf("lb: %d, ub: %d\n", lb_idx, ub_idx);
-    int total_expense = 0;
-    for (int i=lb_idx; i<=ub_idx; i++){
-        total_expense += expense[i];
-    }
+        scanf("%[^\n]s", ub);
 
-    printf("\n%d", total_expense);
+        int ub_idx = 0;
+        int lb_idx = 0;
+
+        for (int i=0; i<12; i++){
+            if (strcmp(months[i], ub) == 0){
+                ub_idx = i;
+            }
+            if (strcmp(months[i], lb) == 0){
+                lb_idx = i;
+            }
+        }
+        printf("lb: %d, ub: %d\n", lb_idx, ub_idx);
+        int total_expense = 0;
+        for (int i=lb_idx; i<=ub_idx; i++){
+            total_expense += expense[i];
+        }
+
+        printf("\n%d\n", total_expense);
+    }
 
     return 0;
 }
+
+/*
+Take 5 strings from user and print them
+*/
