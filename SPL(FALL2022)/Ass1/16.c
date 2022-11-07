3558741d43c4158a91fb11342a57d5b29e806406
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2022-11-07 18:11:18 +0600

    Create 16.c

---
+++ SPL(FALL2022)/Ass1/16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    for (int i=1; i<=n; i++){
+        printf("%d", i);
+        if (i != n)
+            printf(",");
+    }
+
+    return 0;
+}
