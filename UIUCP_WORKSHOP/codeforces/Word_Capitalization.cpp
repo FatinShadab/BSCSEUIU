e9c96d30a8b790ae02d9e7752ce66bc0df943dff
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2022-12-08 00:16:11 +0600

    Create Word_Capitalization.cpp

---
+++ UIUCP_WORKSHOP/codeforces/Word_Capitalization.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    string str;
+    getline(cin, str);
+
+    str[0] = toupper(str[0]);
+
+    cout<<str<<endl;
+
+    return 0;
+}
