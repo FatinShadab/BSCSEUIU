a167a9638c560944291e8d270347d1627ef929fb
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2023-03-09 09:32:49 +0600

    Create Test.java

---
+++ OOP(Spring2023)/Assignment(I)/QVIII/Test.java
@@ -0,0 +1,32 @@
+class Book{
+    void description(){
+        // ; added to fix the error
+        System.out.println("Books are awesome");
+    }
+
+    void putPageMarker(int pageNo){
+        System.out.println("To avoid error");
+    }
+}
+
+class PaperBackBook extends Book{
+    void putPageMarker(int pageNo){
+        System.out.println("Page marker was " + 
+        "put on page no: " + pageNo);
+    }
+}
+
+// need to do the Test class public
+public class Test{
+    public static void main(String[] args){
+        Book book1, book2;
+
+        book1 = new Book();
+        book2 = new PaperBackBook();
+
+        book1.description();
+        book2.description();
+        // need to pass a int to avoid error
+        book2.putPageMarker(10);
+    }
+}
