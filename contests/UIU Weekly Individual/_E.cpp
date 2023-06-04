8dcfdcc674d9793da6ee3f2923b678d23c6cd33b
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2023-06-04 12:52:45 +0600

    Create _E.cpp

---
+++ contests/UIU Weekly Individual/_E.cpp
@@ -0,0 +1,18 @@
+// PB Link : https://vjudge.net/contest/561667#problem/E
+
+#include <bits/stdc++.h>
+using namespace std;
+#define __fastIO__ ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+int main(){
+    __fastIO__
+
+    int T; cin >> T;
+
+    while (T--){
+        int n; cin >> n;
+        cout << n-3 << " 1 1 1\n";
+    }
+
+    return 0;
+}
