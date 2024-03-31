5bb02d1dc3b53a73e51a1b50769f57462d128671
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2024-04-01 00:30:46 +0600

    Create template.cpp
    Creating a cpp template for contest participation

---
+++ contests/template.cpp
@@ -0,0 +1,72 @@
+/*
+                                            ____ ____  ____ 
+                                           / ___|  _ \|  _ \ 
+                                          | |   | |_) | |_) |
+                                          | |___|  __/|  __/  
+                                           \____|_|   |_|
+   ____ _____ _   _ _____ ____      _    _       _____ _____ __  __ ____  _        _  _____ _____ 
+  / ___| ____| \ | | ____|  _ \    / \  | |     |_   _| ____|  \/  |  _ \| |      / \|_   _| ____|
+ | |  _|  _| |  \| |  _| | |_) |  / _ \ | |       | | |  _| | |\/| | |_) | |     / _ \ | | |  _|       
+ | |_| | |___| |\  | |___|  _ <  / ___ \| |___    | | | |___| |  | |  __/| |___ / ___ \| | | |___ 
+  \____|_____|_| \_|_____|_| \_\/_/   \_\_____|   |_| |_____|_|  |_|_|   |_____/_/   \_\_| |_____|
+
+ Name: Md. Fatin Shadab Turja                                                      Id: 011 222 004
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// ============================================================================
+#define __fastIO__ ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ull unsigned long long int
+#define ld long double
+#define ll long long
+#define endl '\n'
+
+// ============================================================================
+#define def typedef struct
+#define v2d vector<vector<int>>
+#define vint vector<int>
+
+// ============================================================================
+#define vsz(v) ((int) v.size())
+#define all(v) (v).begin(), (v).end()
+
+// ============================================================================
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+// ============================================================================
+void fileRW(bool readFile, bool writeFile) {
+    if (readFile) freopen("input.txt", "r", stdin);
+    if (writeFile) freopen("output.txt", "w", stdout);
+}
+
+bool desCMP(int a, int b) {
+    // return's true if 'a' comes before 'b'.
+    // return's false if 'b' comes before 'a'.
+
+    return a > b;
+}
+
+
+// ============================================================================
+void solve() {
+    return;
+}
+
+
+// ============================================================================
+int main() {
+    __fastIO__
+
+    #ifndef ONLINE_JUDGE
+        fileRW(true, true);
+    #endif
+
+    solve();
+
+    return 0;
+}
